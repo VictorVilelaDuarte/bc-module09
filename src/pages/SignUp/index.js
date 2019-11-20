49e15b0ead913b4a83fc94ce6e71beab29b25515
Victor Vilela Duarte <victorvduarte@gmail.com>
2019-11-20 00:37:54 -0300

    Utilizando unform e validação de formulários

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 import logo from '~/assets/logo.svg';
 
+const schema = Yup.object().shape({
+  name: Yup.string().required('Digite seu nome'),
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('E-mail obrigatorio'),
+  password: Yup.string()
+    .min(6, 'Digite no mínimo 6 caracteres')
+    .required('Digite uma senha'),
+});
+
 export default function SingUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="goBarber" />
-      <form>
-        <input type="text" placeholder="Nome completo" />
-        <input type="email" placeholder="Seu e-mail" />
-        <input type="password" placeholder="Sua senha" />
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input type="text" name="name" placeholder="Nome completo" />
+        <Input type="email" name="email" placeholder="Seu e-mail" />
+        <Input type="password" name="password" placeholder="Sua senha" />
         <button type="submit"> Criar conta </button>
         <Link to="/">Já tenho uma conta</Link>
-      </form>
+      </Form>
     </>
   );
 }
--- src/pages/SingIn/index.js
+++ src/pages/SingIn/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 import logo from '~/assets/logo.svg';
 
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('E-mail obrigatorio'),
+  password: Yup.string().required('Digite uma senha'),
+});
+
 export default function SingIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="goBarber" />
-      <form>
-        <input type="email" placeholder="Seu e-mail" />
-        <input type="password" placeholder="Sua senha" />
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input type="email" name="email" placeholder="Seu e-mail" />
+        <Input type="password" name="password" placeholder="Sua senha" />
         <button type="submit"> Acessar </button>
         <Link to="/register">Criar conta gratuita</Link>
-      </form>
+      </Form>
     </>
   );
 }
