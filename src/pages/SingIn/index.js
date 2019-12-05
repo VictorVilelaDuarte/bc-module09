cd162d92a82b835b747b92e1bd0ee08dfe023452
Victor Vilela Duarte <victorvduarte@gmail.com>
2019-12-05 01:01:28 -0300

    Loading da autenticação

--- src/pages/SingIn/index.js
+++ src/pages/SingIn/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
@@ -16,6 +16,7 @@ const schema = Yup.object().shape({
 
 export default function SingIn() {
   const dispatch = useDispatch();
+  const loading = useSelector(state => state.auth.loading);
 
   function handleSubmit({ email, password }) {
     dispatch(signInRequest(email, password));
@@ -27,7 +28,7 @@ export default function SingIn() {
       <Form schema={schema} onSubmit={handleSubmit}>
         <Input type="email" name="email" placeholder="Seu e-mail" />
         <Input type="password" name="password" placeholder="Sua senha" />
-        <button type="submit"> Acessar </button>
+        <button type="submit"> {loading ? 'Carregando...' : 'Acessar'} </button>
         <Link to="/register">Criar conta gratuita</Link>
       </Form>
     </>
