d04e615dfb8de4c7be76480f9e194685591b2082
Victor Vilela Duarte <victorvduarte@gmail.com>
2019-12-07 20:25:36 -0300

    Informações no Header, logout e bugfixed: Alteração de usuario

--- src/pages/Profile/AvatarInput/index.js
+++ src/pages/Profile/AvatarInput/index.js
@@ -1,16 +1,15 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { useField } from '@rocketseat/unform';
 import api from '~/services/api';
-
 import { Container } from './styles';
 
 export default function AvatarInput() {
+  const ref = useRef();
+
   const { defaultValue, registerField } = useField('avatar');
 
-  const [file, setFile] = useState(defaultValue && defaultValue.url);
+  const [file, setFile] = useState(defaultValue && defaultValue.id);
   const [preview, setPreview] = useState(defaultValue && defaultValue.url);
-
-  const ref = useRef();
 
   useEffect(() => {
     if (ref.current) {
@@ -24,11 +23,11 @@ export default function AvatarInput() {
 
   async function handleChange(e) {
     const data = new FormData();
+
     data.append('file', e.target.files[0]);
-
     const response = await api.post('files', data);
-
     const { id, url } = response.data;
+
     setFile(id);
     setPreview(url);
   }
